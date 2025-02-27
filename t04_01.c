80e5141b9928b020b4c97725b50052d478133e95
DavinaHutabarat <dggxqh@gmail.com>
2025-02-27 13:53:40 +0700

    pair 06
    up

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,21 @@
-// NIM - Name
-// NIM - Name
+// 12S24047 - Davina Olivia Yosefanny Hutabarat
+// 12S24045 - Mutiara Y.H. Sianturi
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
+int main() {
+    char str[60];
+    scanf("%60s", str);
+    
+    int length = strlen(str);
+    for (int i = 0; i < length; i += 3) {
+        char temp[4] = {str[i], str[i+1], str[i+2], '\0'}; 
+        int ascii_value = atoi(temp); 
+        printf("%c", ascii_value); 
+    }
+    
+    printf("\n");
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,18 @@
-// NIM - Name
-// NIM - Name
+// 12S24045 - Mutiara YH Sianturi
+// 12S24047 - Davina Hutabarat
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+  char input[21];
+  fgets(input, sizeof(input), stdin);
+  input[strcspn(input, "\n")] = 0;
+  for (int i = 0; i < strlen(input); i++) {
+  printf("%03d", input[i]);
+  }
+
+  printf("\n");
 
   return 0;
 }
